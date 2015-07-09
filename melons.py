579c6a09ec3c88db2f803d655a3f050eeed387ce
Hackbright Student <info@hackbrightacademy.com>
2015-07-08 18:05:14 -0700

    fully created Melon class and functions

--- melons.py
+++ melons.py
@@ -4,14 +4,6 @@ class Melon(object):
     def __init__(self, base_price= 5.00):
         self.base_price = base_price
 
-
-class CantaloupeOrder(Melon):
-    species = "Cantaloupe"
-    rind_color = "tan"
-    imported = False
-    shape = 'natural'
-    seasons = ['Fall', 'Summer']
-
     def get_price(self, qty):
         """Calculate price, given a number of melons ordered."""
         if self.imported == True:
@@ -23,9 +15,23 @@ class CantaloupeOrder(Melon):
         if self.species == "Cantaloupe":
             self.base_price = self.base_price * 0.50
 
+        if self.species == "Watermelon" and qty >= 3:
+            self.base_price = self.base_price * 0.75
+
+        if self.species == "Casaba" or self.species == "Ogens":
+            self.base_price += 1
+
         total = float(self.base_price * qty)
 
         return total
+
+class CantaloupeOrder(Melon):
+    species = "Cantaloupe"
+    rind_color = "tan"
+    imported = False
+    shape = 'natural'
+    seasons = ['Fall', 'Summer']
+
 
 class WatermelonOrder(Melon):
     species = "Watermelon"
@@ -34,20 +40,6 @@ class WatermelonOrder(Melon):
     shape = 'natural'
     seasons = ['Fall', 'Summer']
 
-    def get_price(self, qty):
-        """Calculate price, given a number of melons ordered."""
-        if self.imported == True:
-            self.base_price = self.base_price * 1.5
-
-        if self.shape == "square":
-            self.base_price = self.base_price * 2
-
-        if self.species == "Watermelon" and qty >= 3:
-            self.base_price = self.base_price * 0.75
-
-        total = float(self.base_price * qty)
-
-        return total
 
 class CasabaOrder(Melon):
     species = "Casaba"
@@ -64,7 +56,7 @@ class CasabaOrder(Melon):
         if self.shape == "square":
             self.base_price = self.base_price * 2
 
-        self.base_price += 1
+
 
         total = float(self.base_price * qty)
 
@@ -173,17 +165,4 @@ class OgenOrder(Melon):
     shape = 'natural'
     seasons = ['Spring', 'Summer']
 
-    def get_price(self, qty):
-        """Calculate price, given a number of melons ordered."""
-        if self.imported == True:
-            self.base_price = self.base_price * 1.5
-
-        if self.shape == "square":
-            self.base_price = self.base_price * 2
-
-        if self.species == "Ogens":
-            self.base_price += 1
-
-        total = float(self.base_price * qty)
-
-        return total
+    
